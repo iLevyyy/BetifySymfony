f3e287c74da0ecb507dd2f5ea3549055ff649179
iLevyyy <alexrodiaz12@gmail.com>
2024-03-13 16:08:22 +0100

    Add tablas canciones historico+algoritmo change

--- Betify/src/Controller/algoritmo.php
+++ Betify/src/Controller/algoritmo.php
@@ -20,8 +20,8 @@ $dvt = $diferenciaVisitasDia3 + $diferenciaVisitasDia2 + $diferenciaVisitasDia1 
 
 //Pendiente == Tasa de crecimiento
 
-$pendientePonderadaCancion1 = pendiente($cancion1vd2,$cancion1vd3)*0.6 + pendiente($cancion1vd1,$cancion1vd2)*0.75 +pendiente($cancion1vd0,$cancion1vd1)*0.9;
-$pendientePonderadaCancion2 = pendiente($cancion2vd2,$cancion2vd3)*0.6 + pendiente($cancion2vd1,$cancion2vd2)*0.75 +pendiente($cancion2vd0,$cancion2vd1)*0.9;
+$pendientePonderadaCancion1 = pendiente($cancion1vd2,$cancion1vd3)*0.7 + pendiente($cancion1vd1,$cancion1vd2)*0.8 +pendiente($cancion1vd0,$cancion1vd1)*0.9;
+$pendientePonderadaCancion2 = pendiente($cancion2vd2,$cancion2vd3)*0.7 + pendiente($cancion2vd1,$cancion2vd2)*0.8 +pendiente($cancion2vd0,$cancion2vd1)*0.9;
 
 $pendienteSinPonderarCancion1 =  pendiente($cancion1vd2,$cancion1vd3) + pendiente($cancion1vd1,$cancion1vd2) +pendiente($cancion1vd0,$cancion1vd1);
 $pendienteSinPonderarCancion2 = pendiente($cancion2vd2,$cancion2vd3) + pendiente($cancion2vd1,$cancion2vd2) +pendiente($cancion2vd0,$cancion2vd1);
@@ -35,35 +35,37 @@ $relacionCrecimientoDia1 = $cancion1vd1/$cancion2vd1;
 $relacionCrecimientoDia0 = $cancion1vd0/$cancion2vd0;
 
 
-echo("La cancion A ha crecido ponderadamente sobre la cancion B un: ".$relacionPonderadaEntrePendientes."<br>");
-echo("La cancion A ha crecido sin ponderar (crecimiento real) sobre la cancion B un: ".$relacionSinPonderarEntrePendientes."<br>");
+// echo("La cancion A ha crecido ponderadamente sobre la cancion B un: ".$relacionPonderadaEntrePendientes."<br>");
+// echo("La cancion A ha crecido sin ponderar (crecimiento real) sobre la cancion B un: ".$relacionSinPonderarEntrePendientes."<br>");
 
-//La variable de arriba nace de las 4 siguientes
-echo("El dia -3, la cancion A tuvo ".$relacionCrecimientoDia3." visitas"." por cada visita de la cancion B". "<br>");
-echo("El dia -2, la cancion A tuvo ".$relacionCrecimientoDia2." visitas"." por cada visita de la cancion B". "<br>");
-echo("El dia -1, la cancion A tuvo ".$relacionCrecimientoDia1." visitas"." por cada visita de la cancion B". "<br>");
-echo("El dia -0, la cancion A tuvo ".$relacionCrecimientoDia0." visitas"." por cada visita de la cancion B". "<br>");
+// //La variable de arriba nace de las 4 siguientes
+// // echo("El dia -3, la cancion A tuvo ".$relacionCrecimientoDia3." visitas"." por cada visita de la cancion B". "<br>");
+// // echo("El dia -2, la cancion A tuvo ".$relacionCrecimientoDia2." visitas"." por cada visita de la cancion B". "<br>");
+// // echo("El dia -1, la cancion A tuvo ".$relacionCrecimientoDia1." visitas"." por cada visita de la cancion B". "<br>");
+// // echo("El dia -0, la cancion A tuvo ".$relacionCrecimientoDia0." visitas"." por cada visita de la cancion B". "<br>");
 
-$relacionCrecimientoTotalCancion1 = $cancion1vd0/$cancion1vd3;
-$relacionCrecimientoTotalCancion2 = $cancion2vd0/$cancion2vd3;
+// $relacionCrecimientoTotalCancion1 = $cancion1vd0/$cancion1vd3;
+// $relacionCrecimientoTotalCancion2 = $cancion2vd0/$cancion2vd3;
+// $relacionCrecimientoEntreCanciones = $relacionCrecimientoTotalCancion1/$relacionCrecimientoTotalCancion2;
 
-echo("La cancion 1/A ha ha tenido una tasa de crecimiento de: ".$relacionCrecimientoTotalCancion1."<br>");
-echo("La cancion 2/B ha ha tenido una tasa de crecimiento de: ".$relacionCrecimientoTotalCancion2."<br>");
+// echo("La cancion 1/A ha ha tenido una tasa de crecimiento de: ".$relacionCrecimientoTotalCancion1."<br>");
+// echo("La cancion 2/B ha ha tenido una tasa de crecimiento de: ".$relacionCrecimientoTotalCancion2."<br>");
+// echo("Tasa de crecimiento entre canciones (la cancion A ha crecido sobre la B: ".$relacionCrecimientoEntreCanciones);
+// //Si este numero es negativo, significa que la cancion B ha tenido mas visitas en los ultimos 3 dias
+// $diferenciaVisitasTotal = $diferenciaVisitasDia0 - $diferenciaVisitasDia3; 
 
-//Si este numero es negativo, significa que la cancion B ha tenido mas visitas en los ultimos 3 dias
-$diferenciaVisitasTotal = $diferenciaVisitasDia0 - $diferenciaVisitasDia3; 
+// echo("En total, la diferencia de visitas de la cancion A respecto a la cancion B se ha visto alterada en: ".$diferenciaVisitasTotal."<br>");
+// echo("La diferencia de visitas restante/actual es de: ".$diferenciaVisitasDia0."<br>");
+// //$cuota = $relacionPonderadaEntrePendientes
 
-echo("En total, la diferencia de visitas de la cancion A respecto a la cancion B se ha visto alterada en: ".$diferenciaVisitasTotal."<br>");
-echo("La diferencia de visitas restante/actual es de: ".$diferenciaVisitasDia0."<br>");
-//$cuota = $relacionPonderadaEntrePendientes
+// $cuota =  $diferenciaVisitasDia0 * $relacionCrecimientoEntreCanciones;
 
-$cuota =  $diferenciaVisitasDia0/ $diferenciaVisitasTotal * $relacionPonderadaEntrePendientes;
+// echo("Cuota de que la cancion A es superada por la cancion B: ".$cuota);
 
-echo("Cuota de que la cancion A es superada por la cancion B: ".$cuota);
+// //Esta funcion es especifica a este contexto, ya que de ser un punto real, debería ser un array con dos valores
+// //Sin embargo, como la diferencia en el eje X siempre es de 1 dia, la operacion se puede simplificar a una sencilla resta
+// //La función no es necesaria, pero esta implementada para genera unas formulas mas legibles
 
-//Esta funcion es especifica a este contexto, ya que de ser un punto real, debería ser un array con dos valores
-//Sin embargo, como la diferencia en el eje X siempre es de 1 dia, la operacion se puede simplificar a una sencilla resta
-//La función no es necesaria, pero esta implementada para genera unas formulas mas legibles
 function pendiente($punto1, $punto2 ){ 
     return($punto2-$punto1);
 }
